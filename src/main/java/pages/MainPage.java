1654a0d5f0393dffae5a3090232d5547a6eec4fd
Angelika <oleinik38@gmail.com>
2020-10-15 23:49:07 +0400

    edrfg

---
+++ src/main/java/pages/MainPage.java
@@ -0,0 +1,37 @@
+package pages;
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MainPage extends BasePage {
+    WebDriver driver;
+
+    public MainPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//*[@id='header']/div[2]/div/div/nav/div[1]/a")
+    private WebElement signInButton;
+
+    @FindBy(xpath = "//*[@id=\"center_column\"]/h1")
+    private WebElement headerTitlePage;
+
+
+    public MainPage openMainPage() {
+        driver.get("http://automationpractice.com/");
+        return this;
+    }
+
+    public AuthenticationPage clickSignIn() {
+        signInButton.click();
+        return new AuthenticationPage(driver);
+    }
+}
+
+
+
